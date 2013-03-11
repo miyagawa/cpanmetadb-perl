300e0d037c7b633d77ad230a1f548a4196de4c20
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-03-11 16:09:04 -0700

    fix script path

--- script/daily.sh
+++ script/daily.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+cd `dirname $0`
 DATE=`date +%Y%m%d`
-mkdir static/versions 2>/dev/null
-tail -100000 access_log | perl script/drawchart.pl > static/versions/index.html
-cp static/versions/index.html static/versions/$DATE.html
+mkdir ../static/versions 2>/dev/null
+tail -100000 ../access_log | perl drawchart.pl > ../static/versions/index.html
+cp ../static/versions/index.html ../static/versions/$DATE.html
