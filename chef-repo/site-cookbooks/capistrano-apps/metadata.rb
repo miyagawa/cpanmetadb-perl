cb8f982b7ce7909660e77bc46146172dfaca4925
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-07-09 23:08:55 -0700

    remove vagrant/chef. Keep capistrano

--- chef-repo/site-cookbooks/capistrano-apps/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'capistrano-apps'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures capistrano-apps'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
