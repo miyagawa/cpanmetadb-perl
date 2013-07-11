e722fc78217801222936ca63f8f121c392403f08
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-07-11 15:50:00 -0700

    fix path for access_log

--- script/daily.sh
+++ script/daily.sh
@@ -5,7 +5,7 @@ source /u/apps/cpanmetadb-perl/shared/.env
 DATE=`date +%Y%m%d`
 TMPFILE=/tmp/$DATE.$$.js
 
-tail -100000 access_log | carton exec script/drawchart.pl > $TMPFILE
+tail -100000 log/access_log | carton exec script/drawchart.pl > $TMPFILE
 
 carton exec script/s3-upload.pl $TMPFILE stats/current.js application/javascript
 carton exec script/s3-upload.pl $TMPFILE stats/$DATE.js application/javascript
